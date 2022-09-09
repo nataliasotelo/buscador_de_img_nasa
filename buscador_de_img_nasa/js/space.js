fc110684bfbb8db616eacf8bd9ba27964b4494b4
nataliasotelo <110506914+nataliasotelo@users.noreply.github.com>
2022-09-09 11:03:28 -0300

    elimine comentarios en space.js

--- buscador_de_img_nasa/js/space.js
+++ buscador_de_img_nasa/js/space.js
@@ -65,37 +65,4 @@ const showList = async() => {
      
 }
 
-/* function showConstelacionList(array){
-    let htmlContentToAppend = "";
-
-    for(let i = 0; i < array.length; i++){ 
-        let category = array[i];
-
-        htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                    <img src=" ${category.media_type} " alt="product image" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <div class="mb-1">
-                        <h4> ${category.title} - ${category.date_created} </h4> 
-                        <p> ${category.description} </p> 
-                        </div>
-                        <small class="text-muted"> ${category.soldCount} vendidos</small> 
-                    </div>
-                </div>
-            </div>
-        </div>
-        `
-        document.getElementById("contenedor").innerHTML = htmlContentToAppend;    }
- }
- */
-
- function showConstelacionList(array){
-    
- }
-
-
 
